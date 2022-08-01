e1afe0311e28af4f97ebf3d12fb16a17d59ca4e6
FreyGeospatial <jordan@freygeospatial.com>
2022-08-01 17:27:23 -0400

    created program.cs and updated readme

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONSerializationAndDeserialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Serialization");
+            Movie movie = new Movie { Id = 1, Title = "Mission Impossible" };
+            string result = JsonConvert.SerializeObject(movie);
+            Console.WriteLine(result);
+        }
+    }
+    class Movie
+    {
+        public int Id { get; set; }
+        public string Title {get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # csharp_basics
 I need to learn some basic C# for my job, so this is where I'll keep my learning materials
+
+## to build and run:
+- `dotnet build`
+- `dotnet run`
+
+If this doesn't work, try running `dotnet new console` prior to these steps.
